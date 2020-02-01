08ac318391738107f8e32ec7632cc2c7cc300567
Artem Feshchenko <artfeshchenko@gmail.com>
2020-02-01 14:21:12 +0200

    Finish Baseboard Information binding

--- board_feature.go
+++ board_feature.go
@@ -8,12 +8,14 @@ var _BoardFeature = _FlagDictionary64{
 	"Board is hot swappable",
 }
 
+// BoardFeature collection of flags that identify features of this baseboard.
 type BoardFeature byte
 
 func (b BoardFeature) String() string {
 	return _BoardFeature.str(uint64(b))
 }
 
+// Slice returns list of flags.
 func (b BoardFeature) Slice() []string {
 	return _BoardFeature.slice(uint64(b))
 }
